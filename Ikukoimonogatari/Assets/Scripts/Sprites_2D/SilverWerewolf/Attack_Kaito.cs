8b453ae983621e6183183287abc02c4d9c802a47
MiyuriAmari <miyuri.amari@gmail.com>
2018-12-02 19:42:11 +0900

    とりあえず保存します。

---
+++ Ikukoimonogatari/Assets/Scripts/Sprites_2D/SilverWerewolf/Attack_Kaito.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Kaito : MonoBehaviour
+{
+    //種類
+    public enum Kind
+    {
+        Akatsuki,//海斗
+        Enemy,//敵
+    };
+
+    //攻撃対象
+    public Kind AttackObject;
+
+    BattleManager manager;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = FindObjectOfType<BattleManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 自身の敵を攻撃
+    /// </summary>
+    public void AttackMyEnemy()
+    {
+        switch (AttackObject)
+        {
+            case Kind.Kaito:
+                AttackKaitoi();
+                break;
+
+            case Kind.Enemy:
+                AttackEnemey();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 海斗に攻撃
+    /// </summary>
+    void AttackKaito()
+    {
+        manager.DamageKaito(50);
+    }
+
+    /// <summary>
+    /// 敵に攻撃
+    /// </summary>
+    void AttackEnemey()
+    {
+        manager.DamageEnemy(50);
+    }
+}
