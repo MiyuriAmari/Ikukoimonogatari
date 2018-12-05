8eadc7bebd0b4ca17341c44258a86e0fdd831c98
MiyuriAmari <miyuri.amari@gmail.com>
2018-12-05 18:54:14 +0900

    精霊バトルシーンアニメーション仮完成しました。

--- Ikukoimonogatari/Assets/Scripts/Sprites_2D/HPGauge.cs
+++ Ikukoimonogatari/Assets/Scripts/Sprites_2D/HPGauge.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HPGauge : MonoBehaviour {
+public class HPGauge: MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
